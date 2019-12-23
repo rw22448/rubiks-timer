523c6004d2de2d45f603c57b2107988af07bea0b
s3720241 <rw22448@gmail.com>
2019-12-23 18:47:18 +1100

    Refactor Stopwatch to be functional component

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,45 +1,42 @@
-import React, { Component, useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
-class Stopwatch extends Component {
-  state = {
-    status: false,
-    runningTime: 0
-	};
-	
-  handleClick = () => {
-    this.setState(state => {
-      if (state.status) {
-        clearInterval(this.timer);
-      } else {
-        const startTime = Date.now() - this.state.runningTime;
-        this.timer = setInterval(() => {
-          this.setState({ runningTime: Date.now() - startTime });
-        });
-      }
-      return { status: !state.status };
-    });
-	};
-	
-  handleReset = () => {
-		clearInterval(this.timer);
-    this.setState({ runningTime: 0, status: false });
-	};
+const Stopwatch = () => {
+  const [status, setStatus] = useState(false);
+  const [runningTime, setRunningTime] = useState(0);
+  let timer = useRef(null);
 
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	};
-	
-  render() {
-		const { status, runningTime } = this.state;
-		
-    return (
-      <div>
-        <p>{runningTime}ms</p>
-        <button onClick={this.handleClick}>{status ? 'Stop' : 'Start'}</button>
-        <button onClick={this.handleReset}>Reset</button>
-      </div>
-    );
+  const handleClick = () => {
+    if (status) {
+      clearInterval(timer.current);
+    } else {
+      const startTime = Date.now() - runningTime;
+      timer.current = setInterval(() => {
+        setRunningTime(Date.now() - startTime);
+      });
+    }
+
+    setStatus(!status);
   };
+
+  const handleReset = () => {
+    clearInterval(timer.current);
+    setStatus(false);
+    setRunningTime(0);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timer.current)
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>{runningTime}ms</p>
+      <button onClick={handleClick}>{status ? 'Stop' : 'Start'}</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
 };
 
 export default Stopwatch;
